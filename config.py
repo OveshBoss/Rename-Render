c638db1e54efb2c1d07f30cb80e4dde111d2daf7
ovesh boss <oveshboss.08@gmail.com>
2025-12-27 15:16:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "23903140")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "579f1bcf3eac1660d81ef34b09906012")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
@@ -27,7 +27,7 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1416433622').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
